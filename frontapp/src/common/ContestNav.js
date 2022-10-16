93f31a94371decf864e1f1ad7c24161d62887b79
supersloy <supersloy@yandex.ru>
2022-10-16 15:27:28 +0300

    feat: Redirect on tasks page

--- frontapp/src/common/ContestNav.js
+++ frontapp/src/common/ContestNav.js
@@ -1,8 +1,15 @@
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
-import { useEffect } from "react";
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 
-export default function ContestNav({current}) {
+export default function ContestNav({ current }) {
+  const navigate = useNavigate();
+
+  function onClick() {
+    navigate("/tasks")
+  }
+
   return (
     <ButtonGroup
       variant="outlined"
@@ -10,7 +17,9 @@ export default function ContestNav({current}) {
       size="large"
       color="warning"
     >
-      <Button disabled={current === "tasks"}> Tasks</Button>
+      <Button disabled={current === "tasks"} onClick={() => onClick()}>
+        Tasks
+      </Button>
       <Button disabled={current === "sumbit"}> Submit solution</Button>
       <Button disabled={current === "submissions"}> My submissions</Button>
     </ButtonGroup>
