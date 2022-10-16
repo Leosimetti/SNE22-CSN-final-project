3fbd19bea028e4b2eb4136663f0a7a4014f1bcc7
nikololiahim <olomishcak@outlook.com>
2022-10-16 15:56:11 +0300

    added boilerplate for rabbitmq

--- app/build.sbt
+++ app/build.sbt
@@ -2,22 +2,23 @@ ThisBuild / version := "0.1.0-SNAPSHOT"
 
 ThisBuild / scalaVersion := "2.13.10"
 
-
-val http4sVersion = "0.23.10"
+val http4sVersion = "0.23.16"
+val rabbitMqVersion = "5.0.0"
 
 lazy val root = (project in file("."))
   .settings(
     name := "CSN-final-project",
-    scalacOptions += "-Wnonunit-statement"
+    scalacOptions += "-Wnonunit-statement",
   )
 
 libraryDependencies ++= Seq(
   "com.github.fd4s" %% "fs2-kafka" % "3.0.0-M8",
   "org.slf4j" % "slf4j-simple" % "2.0.3",
-
   "com.softwaremill.sttp.tapir" %% "tapir-core" % "1.1.2",
   "com.softwaremill.sttp.tapir" %% "tapir-http4s-server" % "1.1.2",
-
   "org.http4s" %% "http4s-ember-server" % http4sVersion,
-  "org.http4s" %% "http4s-ember-client" % http4sVersion
+  "org.http4s" %% "http4s-ember-client" % http4sVersion,
+  "dev.profunktor" %% "fs2-rabbit" % rabbitMqVersion,
+  "dev.profunktor" %% "fs2-rabbit-circe" % rabbitMqVersion,
+  "io.circe" %% "circe-generic" % "0.14.3",
 )
---
+++ app/src/main/scala/rabbitmq/Main.scala
@@ -0,0 +1,77 @@
+package rabbitmq
+
+import cats.data.Kleisli
+import cats.effect._
+import cats.implicits._
+import dev.profunktor.fs2rabbit.config.declaration.DeclarationQueueConfig
+import dev.profunktor.fs2rabbit.interpreter.RabbitClient
+import dev.profunktor.fs2rabbit.json.Fs2JsonEncoder
+import dev.profunktor.fs2rabbit.model.AckResult.Ack
+import dev.profunktor.fs2rabbit.model.AmqpFieldValue.{LongVal, StringVal}
+import dev.profunktor.fs2rabbit.model._
+import fs2.{Pipe, Stream}
+
+import java.nio.charset.StandardCharsets.UTF_8
+
+class AutoAckFlow[F[_]: Async, A](
+    consumer: Stream[F, AmqpEnvelope[A]],
+    logger: Pipe[F, AmqpEnvelope[A], AckResult],
+    publisher: AmqpMessage[String] => F[Unit],
+) {
+  import io.circe.generic.auto._
+  case class Address(number: Int, streetName: String)
+  case class Person(id: Long, name: String, address: Address)
+
+  private val jsonEncoder = new Fs2JsonEncoder
+  import jsonEncoder.jsonEncode
+
+  val jsonPipe: Pipe[F, AmqpMessage[Person], AmqpMessage[String]] =
+    _.covary[F].map(jsonEncode[Person])
+
+  val simpleMessage: AmqpMessage[String] =
+    AmqpMessage(
+      "Hey!",
+      AmqpProperties(headers = Map("demoId" -> LongVal(123), "app" -> StringVal("fs2RabbitDemo"))),
+    )
+  val classMessage: AmqpMessage[Person] = AmqpMessage(
+    Person(1L, "Sherlock", Address(212, "Baker St")),
+    AmqpProperties.empty,
+  )
+
+  val flow: Stream[F, Unit] =
+    Stream(
+      Stream(simpleMessage).covary[F].evalMap(publisher),
+      Stream(classMessage).covary[F].through(jsonPipe).evalMap(publisher),
+      consumer.through(logger).evalMap(ack => Async[F].delay(println(ack))),
+    ).parJoin(3)
+
+}
+
+class AutoAckConsumerDemo[F[_]: Async](R: RabbitClient[F]) {
+
+  private val queueName = QueueName("testQ")
+  private val exchangeName = ExchangeName("testEX")
+  private val routingKey = RoutingKey("testRK")
+  implicit val stringMessageEncoder: Kleisli[F, AmqpMessage[String], AmqpMessage[Array[Byte]]] =
+    Kleisli(s => s.copy(payload = s.payload.getBytes(UTF_8)).pure[F])
+
+  def logPipe: Pipe[F, AmqpEnvelope[String], AckResult] = _.evalMap { amqpMsg =>
+    Async[F].delay(println(s"Consumed: $amqpMsg")).as(Ack(amqpMsg.deliveryTag))
+  }
+
+  val program: F[Unit] = R.createConnectionChannel.use { implicit channel =>
+    for {
+      _ <- R.declareQueue(DeclarationQueueConfig.default(queueName))
+      _ <- R.declareExchange(exchangeName, ExchangeType.Topic)
+      _ <- R.bindQueue(queueName, exchangeName, routingKey)
+      publisher <- R
+        .createPublisher[AmqpMessage[String]](exchangeName, routingKey)
+      consumer <- R.createAutoAckConsumer[String](queueName)
+      _ <- new AutoAckFlow[F, String](
+        consumer,
+        logPipe,
+        publisher,
+      ).flow.compile.drain
+    } yield ()
+  }
+}
