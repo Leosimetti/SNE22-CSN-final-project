46a76a9176523759cbb4d87e69c8b74dd4471b14
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2022-10-17 20:55:14 +0300

    feat(app:shared): added decoders

--- app/src/main/scala/shared/package.scala
+++ app/src/main/scala/shared/package.scala
@@ -1,6 +1,10 @@
 package shared
 
+import io.circe.Decoder
+
 import scala.concurrent.duration.Duration
+import io.circe.generic.semiauto.deriveDecoder
+import org.latestbit.circe.adt.codec._
 
 object types {
   type UserId = String
@@ -14,10 +18,18 @@ sealed trait Language
 
 object Language {
   case object Python extends Language
+
+  implicit val decoder: Decoder[Language] = JsonTaggedAdtCodec.createPureEnumDecoder[Language]()
 }
 
 case class ReferenceSolution(code: String, language: Language)
+object ReferenceSolution {
+  implicit val decoder: Decoder[ReferenceSolution] = deriveDecoder
+}
 case class UserSolution(userId: UserId, code: String, language: Language)
+object UserSolution {
+  implicit val decoder: Decoder[UserSolution] = deriveDecoder
+}
 
 case class Problem(
     problemId: ProblemId,
@@ -25,11 +37,17 @@ case class Problem(
     referenceSolutions: List[ReferenceSolution],
     executionTimeThreshold: ExecutionTimeThreshold,
 )
+object Problem {
+  implicit val decoder: Decoder[Problem] = deriveDecoder
+}
 
 case class Task(
     problem: Problem,
     userSolution: UserSolution,
 )
+object Task {
+  implicit val decoder: Decoder[Task] = deriveDecoder
+}
 
 sealed trait Result {
   val userId: UserId
@@ -37,7 +55,14 @@ sealed trait Result {
 }
 
 object Result {
+
   case class Success(
+      duration: Duration,
+      override val userId: UserId,
+      override val problemId: ProblemId,
+  ) extends Result
+
+  case class Failure(
       duration: Duration,
       override val userId: UserId,
       override val problemId: ProblemId,
@@ -45,23 +70,23 @@ object Result {
 
   // WrongAnswer
 
-  case class CompilationError(
-      msg: String,
-      override val userId: UserId,
-      override val problemId: ProblemId,
-  ) extends Result
-
-  case class TimeoutError(
-      duration: Duration,
-      override val userId: UserId,
-      override val problemId: ProblemId,
-  ) extends Result
-
-  case class RuntimeError(
-      msg: String,
-      duration: Duration,
-      exitCode: Int,
-      override val userId: UserId,
-      override val problemId: ProblemId,
-  ) extends Result
+//  case class CompilationError(
+//      msg: String,
+//      override val userId: UserId,
+//      override val problemId: ProblemId,
+//  ) extends Result
+//
+//  case class TimeoutError(
+//      duration: Duration,
+//      override val userId: UserId,
+//      override val problemId: ProblemId,
+//  ) extends Result
+//
+//  case class RuntimeError(
+//      msg: String,
+//      duration: Duration,
+//      exitCode: Int,
+//      override val userId: UserId,
+//      override val problemId: ProblemId,
+//  ) extends Result
 }
