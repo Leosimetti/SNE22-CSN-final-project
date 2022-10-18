001a979caf5ec9ac1e2d7dcb0f69a53a19f38b77
Vitaliy <42554566+Leosimetti@users.noreply.github.com>
2022-10-18 23:06:38 +0300

    feat(app:web): added a simple endpoint for submitting solutions

--- app/src/main/scala/web/Main.scala
+++ app/src/main/scala/web/Main.scala
@@ -7,29 +7,36 @@ import sttp.tapir.server.http4s.Http4sServerInterpreter
 import sttp.tapir._
 import cats.effect.IO
 import org.http4s.HttpRoutes
+import shared.UserSubmission
+import sttp.tapir.json.circe._
+import org.http4s.server.middleware._
 
 object Main extends IOApp {
 
-  def countCharacters(s: String): IO[Either[Unit, Int]] =
-    IO.pure(Right[Unit, Int](s.length))
+  def putTaskIntoRabbitMQ(s: UserSubmission): IO[Either[Unit, String]] = {
+    for {
+      _ <- IO.println(s)
+      res <- IO.pure(Right[Unit, String]("VSYO NORM"))
+    } yield res
 
-  val countCharactersEndpoint: PublicEndpoint[String, Unit, Int, Any] =
-    endpoint
-      .post
-      .in(stringBody)
-      .out(plainBody[Int])
+  }
 
-  val countCharactersRoutes: HttpRoutes[IO] =
+  val submitResultEndpoint: PublicEndpoint[UserSubmission, Unit, String, Any] =
+    endpoint.post
+      .in(jsonBody[UserSubmission])
+      .in("submit")
+      .out(plainBody[String])
+
+  val submitResultRoutes: HttpRoutes[IO] =
     Http4sServerInterpreter[IO]()
-      .toRoutes(countCharactersEndpoint.serverLogic(countCharacters))
-
+      .toRoutes(submitResultEndpoint.serverLogic(putTaskIntoRabbitMQ))
 
   override def run(args: List[String]): IO[ExitCode] =
     EmberServerBuilder
       .default[IO]
       .withHost(ipv4"0.0.0.0")
       .withPort(port"1488")
-      .withHttpApp(countCharactersRoutes.orNotFound)
+      .withHttpApp(CORS.policy.withAllowOriginAll(submitResultRoutes).orNotFound)
       .build
       .useForever
       .as(ExitCode.Success)
