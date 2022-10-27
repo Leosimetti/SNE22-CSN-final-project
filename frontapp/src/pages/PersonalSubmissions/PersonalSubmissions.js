c84a0e83f9ea7d23ba32898ee324120099d6528c
supersloy <supersloy@yandex.ru>
2022-10-27 19:01:25 +0300

    feat: Layer with code in MySubmission page

--- frontapp/src/pages/PersonalSubmissions/PersonalSubmissions.js
+++ frontapp/src/pages/PersonalSubmissions/PersonalSubmissions.js
@@ -1,11 +1,15 @@
 import { Paragraph, Select } from "grommet";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 
 import ContestNav from "../../common/ContestNav";
 import PersonalSubmissionsTable from "./PersonalSubmissionsTable";
+import SubmissionModal from "./SubmissionModal";
 
 export default function personalSubmissions() {
   const [task, setTask] = useState();
+  const [showSubmission, setShowSubmission] = useState(false);
+
+  const onClose = useCallback(()=> {setShowSubmission(false)}, [])
 
   const TASK_OPTIONS = [
     "A-Josko posrat",
@@ -31,8 +35,27 @@ export default function personalSubmissions() {
           onChange={({ option }) => setTask(option)}
           style={{ width: "300px" }}
         />
+        <button onClick={()=>setShowSubmission(true)}></button>
       </div>
-      <PersonalSubmissionsTable></PersonalSubmissionsTable>
+      <SubmissionModal show={showSubmission} code={`keys_list = ['A', 'B', 'C']
+values_list = ['blue', 'red', 'bold']
+
+#There are 3 ways to convert these two lists into a dictionary
+#1- Using Python's zip, dict functionz
+dict_method_1 = dict(zip(keys_list, values_list))
+
+#2- Using the zip function with dictionary comprehensions
+dict_method_2 = {key:value for key, value in zip(keys_list, values_list)}
+
+#3- Using the zip function with a loop
+items_tuples = zip(keys_list, values_list) 
+dict_method_3 = {} 
+for key, value in items_tuples: 
+    if key in dict_method_3: 
+        pass # To avoid repeating keys.
+    else: 
+        dict_method_3[key] = value`} onClose={onClose}/>
+      <PersonalSubmissionsTable/>
     </>
   );
 }
--- frontapp/src/pages/PersonalSubmissions/SubmissionModal.js
+++ frontapp/src/pages/PersonalSubmissions/SubmissionModal.js
@@ -1,5 +1,39 @@
-import { Layer } from "grommet";
+import CodeEditor from "@uiw/react-textarea-code-editor";
+import { Box, Button, Layer } from "grommet";
+import { Close } from "grommet-icons";
+import { useEffect } from "react";
 
-export default function SubmissionModal() {
-  return <Layer></Layer>;
+export default function SubmissionModal({ show, code, onClose, language }) {
+  useEffect(() => {
+    console.log(code);
+  });
+
+  language ??= "python"
+  return (
+    <>
+      {show && (
+        <Layer onEsc={() => onClose()} onClickOutside={() => onClose()}>
+          <Box pad="medium" animation={{ type: "fadeIn", duration: 700 }}>
+            <Button icon={<Close />} onClick={onClose} />
+            <div data-color-mode="light" style={{padding: "10px 0 10px 0"}}>
+              <CodeEditor
+                value={code}
+                language={language}
+                placeholder=""
+                padding={15}
+                style={{
+                  fontSize: 20,
+                  fontFamily:
+                    "ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace",
+                  background: "#F3F3F3",
+                  minHeight: "150px",
+                  minWidth: "700px",
+                }}
+              />
+            </div>
+          </Box>
+        </Layer>
+      )}
+    </>
+  );
 }
